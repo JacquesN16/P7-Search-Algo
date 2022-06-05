d73582402c848ec8537871df34f3c4d0976cde13
Jacques <j.nguyen14216@gmail.com>
2022-06-05 23:37:00 +0200

    search algo 1

--- js/api.js
+++ js/api.js
@@ -1,12 +1,36 @@
 import recipes from './recipes.js';
 
-
+const isSearchWordExist = (text, searchKey) => text.toLowerCase().includes(searchKey);
 let _filteredRecipes = recipes;
-class recipesAPI {
-    // static getAllRecipes = () => {
 
-    //     return recipes
-    // }
+
+const searchOnRecipes = (searchingList, searchKey) => {
+    const filteredRecipes = [];
+    
+    searchKey = searchKey.toLowerCase();
+    for (let i = 0; i < searchingList.length; i++) {
+        const recipe = searchingList[i];
+        if (isSearchWordExist(recipe.name, searchKey)) {
+            filteredRecipes.push(recipe);
+        } else if (isSearchWordExist(recipe.description, searchKey)) {
+            filteredRecipes.push(recipe);
+        } else {
+            for (let y = 0; y < recipe.ingredients.length; y++) {
+                const ingredient = recipe.ingredients[y];
+                if (isSearchWordExist(ingredient.ingredient, searchKey)) {
+                    filteredRecipes.push(recipe);
+                    break;
+                }
+            }
+        }
+    }
+
+    return filteredRecipes;
+};
+
+
+class recipesAPI {
+    
 
     static getRecipes = (filterParams) => {
         if (!filterParams) {
